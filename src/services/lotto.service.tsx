6c4cef1419ec51414629803f6d4554e424ca6152
akarat <akrs33@gmail.com>
2021-02-17 17:23:02 +0700

    Dev: lotto page

--- src/services/lotto.service.tsx
+++ src/services/lotto.service.tsx
@@ -1,7 +1,8 @@
 
 import { LottoCustomer } from '../types/app.type'
 import getUrl from '../common/helpers'
-
+import https from 'https';
+import axios from 'axios';
 // export const Auth = {
 //     isAuthenticated: false,
 //     //isAuthenticated: true,
@@ -15,28 +16,67 @@ import getUrl from '../common/helpers'
 //     }  
 // }
 
-export async function registerLottoCustomer( lottoCustomer: LottoCustomer) {
-    const requestOptions = {
-        method: 'POST',
-        headers: { 
+// At instance level
+// const instance = axios.create({
+//     httpsAgent: new https.Agent({  
+//       rejectUnauthorized: false
+//     })
+//   });
+  
+const axiosRequest = async(lottoCustomer: LottoCustomer):Promise<any> => {
+    return new Promise((resolve,reject) => {
+        const agent = new https.Agent({  
+            rejectUnauthorized: false
+           });
+        
+        const headers = { 
             'Content-Type': 'application/json',
             'client_id': 'minorfoodit'
-        },
-        body: JSON.stringify(lottoCustomer)
-    }
+        }
+    
+        axios.post(getUrl('/v1/lotto/webregister'), lottoCustomer, { httpsAgent: agent,timeout: 25000 ,headers: headers} )
+        .then(response => {
+            const data = response.data
+            resolve(data)
+        })
+        .catch((error) => {
+            reject(error)
+        })
+    })
+}
+
+
+export async function registerLottoCustomer( lottoCustomer: LottoCustomer) {
     try {
-        alert(getUrl('/v1/lotto/webregister'))
-        const response = await fetch(getUrl('/v1/lotto/webregister'), requestOptions)
-        const data = await response.json()
-        alert(data)
-        // if (!response.ok) {
-        //     const error = (data && data.message) || response.status
-        //     return Promise.resolve(error)
-        // }
-        return data
+        let results = await axiosRequest(lottoCustomer)
+        return Promise.resolve(results)
     } catch (error_1) {
         console.error('There was an error!', error_1)
         alert(error_1)
         return Promise.reject(error_1)
     }
+
+    // const requestOptions = {
+    //     method: 'POST',
+    //     headers: { 
+    //         'Content-Type': 'application/json',
+    //         'client_id': 'minorfoodit'
+    //     },
+    //     body: JSON.stringify(lottoCustomer)
+    // }
+    // try {
+    //     alert(getUrl('/v1/lotto/webregister'))
+    //     const response = await fetch(getUrl('/v1/lotto/webregister'), requestOptions)
+    //     const data = await response.json()
+    //     alert(data)
+    //     // if (!response.ok) {
+    //     //     const error = (data && data.message) || response.status
+    //     //     return Promise.resolve(error)
+    //     // }
+    //     return data
+    // } catch (error_1) {
+    //     console.error('There was an error!', error_1)
+    //     alert(error_1)
+    //     return Promise.reject(error_1)
+    // }
 }
